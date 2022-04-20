001bdf711a7d7f2e8b67d2fdd96df94717db9f41
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-04-20 15:48:10 -0500

    nav bar icons clickable for history push

--- src/components/NavBarUser/NavBarUser.jsx
+++ src/components/NavBarUser/NavBarUser.jsx
@@ -1,32 +1,70 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
 
 import { makeStyles } from '@material-ui/core/styles'
 import BottomNavigation from '@material-ui/core/BottomNavigation'
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
 
 import { FaHouseUser } from 'react-icons/fa';
-import { RiCake3Line } from "react-icons/ri";
-import { AiOutlineShop } from "react-icons/ai";
+import { RiCake3Line } from "react-icons/ri";//cupcake
+import { AiOutlineShop } from "react-icons/ai";//storefront
 import { FaDonate } from 'react-icons/fa';
 import { FaUserEdit } from 'react-icons/fa';
 import { AiOutlineUserAdd } from "react-icons/ai";
+import { FaEdit } from 'react-icons/fa';
+import { IoTrashOutline } from "react-icons/io5";
+// import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+
+
+
+const useStyles = makeStyles({
+    root: {
+        width: 370,
+        backgroundColor: "#E0E0E0"
+    }
+
+})
 
 
 
 function NavBarUser() {
 
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+    const history = useHistory();
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue)
+    }
+
+
     return (
-        <>
-            <h1><FaHouseUser /> <RiCake3Line /> <AiOutlineShop /><FaDonate/> <FaUserEdit/>
+        <BottomNavigation
+            className={classes.root}
+            value={value}
+            onChange={(event, newValue) => handleChange(event, newValue)}
+        >
+            <BottomNavigationAction label="Profile" icon={<FaHouseUser size={25} />} 
+            onClick={() => { history.push('/user') }}/>
+
+            <BottomNavigationAction label="Treats" icon={<RiCake3Line size={25} />} 
+            onClick={() => { history.push('/treatList') }}/>
+
+            <BottomNavigationAction label="Bakesales" icon={<AiOutlineShop size={25} />} 
+            onClick={() => { history.push('/bakesale') }}/>
+
+            <BottomNavigationAction label="Donations" icon={<FaDonate size={25} />} 
+            onClick={() => { history.push('/user') }}/>
+
+
+            {/* <RiCake3Line /> <AiOutlineShop /><FaDonate/> <FaUserEdit/>
             
-            <AiOutlineUserAdd/>
-            </h1>
-        </>
+            <AiOutlineUserAdd/> */}
+
+        </BottomNavigation>
     )
 
 }
 
-export default NavBarUser
+export default NavBarUser;
