944b6b5380fb44d00534d41f4129c312fb66fff4
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-04-19 20:53:17 -0500

    install MUI v4

--- src/components/ItemBakeSale/ItemBakeSale.jsx
+++ src/components/ItemBakeSale/ItemBakeSale.jsx
@@ -2,17 +2,17 @@ import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 
-/*/MUI for card
+//MUI for card
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import Typography from '@mui/material/Typography';
+import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 //grid for centering
 import Grid from '@material-ui/core/Grid';
-*/
 
-/*
+
+
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
@@ -26,18 +26,18 @@ const useStyles = makeStyles((theme: Theme) =>
         },
     }),
 );
-*/
+
 
 function ItemBakesale({ bakesale }) {
 
     const history = useHistory();
     const dispatch = useDispatch();
-    // const classes = useStyles(); //for card
+    const classes = useStyles(); //for card
 
 
 
     const handleDetailView = () => {
-        console.log('clicked into HandleDetailView, treat id is:', bakesale.id);
+        // console.log('clicked into HandleDetailView, treat id is:', bakesale.id);
         dispatch({ type: 'FETCH_BAKESALE_DETAIL', payload: bakesale.id })//will trigger fetchDetail saga
         history.push(`/bakesaleDetail/${bakesale.id}`);//'moves' user to page view with treat details
     }
@@ -45,30 +45,30 @@ function ItemBakesale({ bakesale }) {
     return (
         //each card created as the .map loops thru array of treats
         <>
-            {/*<Grid
+            <Grid
                 item md={2}
             >
                 <Card className={classes.root} variant="outlined">
-    <CardContent>*/}
+                    <CardContent>
 
 
-            {/* <Typography>
+                        {/* <Typography>
                         <p key={movie.id} >
                         <h2>{movie.title}</h2></p>
 
                         </Typography> (looks better without the title)*/}
-            <div>
-                <p>{bakesale.org_name}, {bakesale.org_description}</p>
-                <img key={bakesale.id} onClick={handleDetailView}
-                    src="https://fakeimg.pl/300x200/" alt={bakesale.org_name}></img>
-                    <p>{bakesale.org_website} Fundraising goal: {bakesale.fundraising_goal}</p>
-                {/* filler images above for now*/}
-            </div>
+                        <div>
+                            <p>{bakesale.org_name}, {bakesale.org_description}</p>
+                            <img key={bakesale.id} onClick={handleDetailView}
+                                src="https://fakeimg.pl/300x200/" alt={bakesale.org_name}></img>
+                            <p>{bakesale.org_website} Fundraising goal: {bakesale.fundraising_goal}</p>
+                            {/* filler images above for now*/}
+                        </div>
 
 
-            {/*</CardContent>
+                    </CardContent>
                 </Card>
-                    </Grid>*/}
+            </Grid>
         </>
     )
 }
