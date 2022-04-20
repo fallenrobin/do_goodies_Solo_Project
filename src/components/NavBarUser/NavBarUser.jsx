e7ba9cf0016ea163045cfab6228ccc703e87c54c
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-04-20 16:09:46 -0500

    nav bar is sticky to bottom

--- src/components/NavBarUser/NavBarUser.jsx
+++ src/components/NavBarUser/NavBarUser.jsx
@@ -5,6 +5,8 @@ import { useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles'
 import BottomNavigation from '@material-ui/core/BottomNavigation'
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
+import Paper from '@material-ui/core/Paper';
+
 
 import { FaHouseUser } from 'react-icons/fa';
 import { RiCake3Line } from "react-icons/ri";//cupcake
@@ -20,11 +22,26 @@ import { IoTrashOutline } from "react-icons/io5";
 
 const useStyles = makeStyles({
     root: {
-        width: 370,
-        backgroundColor: "#E0E0E0"
-    }
-
+        // width: 370,
+        width: "100%",
+        backgroundColor: "#E0E0E0",
+        position: 'fixed',
+        bottom: 0,
+    },
+    // stickToBottom: {
+        // width: '100%',
+    //     position: 'fixed',
+    //     bottom: 0,
+    // }
 })
+
+// const styles = {
+//     stickToBottom: {
+//       width: '100%',
+//       position: 'fixed',
+//       bottom: 0,
+//     },
+//   };
 
 
 
@@ -40,29 +57,31 @@ function NavBarUser() {
 
 
     return (
-        <BottomNavigation
-            className={classes.root}
-            value={value}
-            onChange={(event, newValue) => handleChange(event, newValue)}
-        >
-            <BottomNavigationAction label="Profile" icon={<FaHouseUser size={25} />} 
-            onClick={() => { history.push('/user') }}/>
+        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+            <BottomNavigation
+                className={classes.root}
+                value={value}
+                onChange={(event, newValue) => handleChange(event, newValue)}
+            >
+                <BottomNavigationAction label="Profile" icon={<FaHouseUser size={25} />}
+                    onClick={() => { history.push('/user') }} />
 
-            <BottomNavigationAction label="Treats" icon={<RiCake3Line size={25} />} 
-            onClick={() => { history.push('/treatList') }}/>
+                <BottomNavigationAction label="Treats" icon={<RiCake3Line size={25} />}
+                    onClick={() => { history.push('/treatList') }} />
 
-            <BottomNavigationAction label="Bakesales" icon={<AiOutlineShop size={25} />} 
-            onClick={() => { history.push('/bakesale') }}/>
+                <BottomNavigationAction label="Bakesales" icon={<AiOutlineShop size={25} />}
+                    onClick={() => { history.push('/bakesale') }} />
 
-            <BottomNavigationAction label="Donations" icon={<FaDonate size={25} />} 
-            onClick={() => { history.push('/user') }}/>
+                <BottomNavigationAction label="Donations" icon={<FaDonate size={25} />}
+                    onClick={() => { history.push('/user') }} />
 
 
-            {/* <RiCake3Line /> <AiOutlineShop /><FaDonate/> <FaUserEdit/>
+                {/* <RiCake3Line /> <AiOutlineShop /><FaDonate/> <FaUserEdit/>
             
             <AiOutlineUserAdd/> */}
 
-        </BottomNavigation>
+            </BottomNavigation>
+        </Paper>
     )
 
 }
