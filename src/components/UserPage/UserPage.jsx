88e822165e93d57cfda90a713f2a42b54f570bcb
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-04-24 20:22:50 -0500

    add styling to User page

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -2,28 +2,62 @@ import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid';
+import { IoLogOutOutline } from "react-icons/io5";
+
+const useStyles = makeStyles({
+  btn: {
+    backgroundColor: "#84C3C8",
+    color: 'black',
+    '&:hover': {
+      backgroundColor: "#42929D"
+    },
+    position: "fixed",
+    bottom: "58px",
+    right: "2px"
+  },
+})
 
 
 function UserPage() {
 
   const user = useSelector((store) => store.user);
   const history = useHistory();
+  const classes = useStyles();
+  const dispatch = useDispatch();
 
   return (
     <div className="container">
 
       <h2>Welcome, {user.username}!</h2>
+      <Grid container
+        spacing={2} //spacing between grid items
+      >
+        <Grid item xs={2} />
+        <Grid
+          item xs={8}>
+          <img src={require("./cakeDecorating.jpeg")} alt={"Baker cat"}
+            style={{ width: 200, height: 200, borderRadius: 400 / 2, zIndex: '7' }} />
+        </Grid>
 
-      
-      <img src={require("./cakeDecorating.jpeg")} alt={"Baker cat"}
-      style={{ width: 200, height: 200, borderRadius: 400 / 2, zIndex: '7' }}/>
+        {/* <p>Your ID is: {user.id}</p> */}
+        <Grid item xs={6}>
 
-
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
-      <button onClick={() => { history.push('/treatList') }}>Treats List</button>
-      {/* <button onClick={history.push('/bakesaleList')}>Bake Sale List</button> */}
-      {/* <button>Donations</button> */}
+        </Grid>
+        {/* <Button variant="contained" color="primary" onClick={() => { history.push('/treatList') }}>Treats List</Button> */}
+        {/* <button onClick={history.push('/bakesaleList')}>Bake Sale List</button> */}
+        {/* <button>Donations</button> */}
+      </Grid>
+      <Button className={classes.btn}
+        variant="contained"
+        color="primary"
+        onClick={() => dispatch({ type: 'LOGOUT' })}>
+        <IoLogOutOutline />
+      </Button>
     </div>
   );
 }
