019c88d538c1ebb945f9eb6645d2eccab82d3cd8
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-04-27 19:24:16 -0500

    center items somewhat on LandingPage

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -6,24 +6,26 @@ import './LandingPage.css';
 import RegisterForm from '../RegisterForm/RegisterForm';
 import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button';
+import { Container } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
 const useStyles = makeStyles({
   page: {
     backgroundColor: "#F8D9D6",
     backgroundSize: 'cover',
-    height: '800px',
-    width: '700px'
+    height: '750px',
+    width: '450px'
   },
   roundImg: {
     width: "300px",
     height: "300px",
     borderRadius: "400" / "2",
     border: "5px double #D9D0F8",
-    backgroundColor:"#F4F4E1"
+    backgroundColor: "#F4F4E1"
   },
   btn: {
-    backgroundColor: "#84C3C8",
-    color: 'black',
+    backgroundColor: "#a8e8ed",
+    color: '#5c5c5c',
     margin: '3px',
     '&:hover': {
       backgroundColor: "#e75480"
@@ -50,22 +52,37 @@ function LandingPage() {
   };
 
   return (
-    <div className={classes.page}>
+    <Grid container
+      className={classes.page}
+      style={{ justifyContent: 'center' }}
+    >
 
-      <img className={classes.roundImg} src="/images/logo.png" alt="Landing page screenshot" />
-
+      <div >
+        <img className={classes.roundImg}
+          style={{ marginTop: '50px' }}
+          src="/images/logo.png" alt="Landing page screenshot" />
+      </div>
       {/* <RegisterForm /> */}
 
-      <Button variant="contained" className={classes.btn} onClick={onLogin}>
-        Login
-      </Button >
-      <Button variant="contained" className={classes.btn} onClick={viewBakeSales}>
-        View bake sales
-      </Button>
-      <p style={{ color: 'blue' }} onClick={() => { history.push('/registration') }}>New user? Click to register</p>
+      <div>
+        <Button variant="contained" className={classes.btn} onClick={onLogin}>
+          Login
+        </Button >
+        <Button variant="contained" className={classes.btn} onClick={viewBakeSales}>
+          View bake sales
+        </Button>
+        <p style={{
+          color: '#5c5c5c',
+          border: '1px solid gray',
+          borderRadius: '6px',
+          padding: '7px',
+          textAlign: 'center'
+        }}
+          onClick={() => { history.push('/registration') }}>New user? Click to register</p>
 
 
-    </div>
+      </div >
+    </Grid>
   );
 }
 
